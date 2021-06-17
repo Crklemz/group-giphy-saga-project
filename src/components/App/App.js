0d7904c533d8e6d04e233fb05a9c8ba419448304
Chris Klemz <crklemz@gmail.com>
2021-06-17 13:18:05 -0500

    pulled master

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Search from '../Search/Search'
 function App(props) {
   return (
+    <Route>
     <div>
       <h1>Giphy Search!</h1>
     </div>
+    <Search />
+    </Route>
   );
 }
 
